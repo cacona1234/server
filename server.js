c5e1695882a95fa687f0c14fe79a0f9f32eec4ee
cacona1234 <122592011+cacona1234@users.noreply.github.com>
2026-02-23 18:20:58 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: process.env.PORT || 8080 });
 
+const historial = [];
+
 wss.on('connection', (ws) => {
+    historial.forEach(msg => ws.send(msg));
+
     ws.on('message', (msg) => {
+        historial.push(msg.toString());
         wss.clients.forEach(client => {
             if (client.readyState === WebSocket.OPEN)
                 client.send(msg.toString());
         });
     });
 });
-
-
-console.log("Servidor corriendo en puerto 8080");
